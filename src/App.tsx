53faa6d8db4d2a14a69d3847561f98e25c62b1f8
Abytova Saikal <saikalabytova1@gmail.com>
2023-06-03 14:17:05 +0600

    Added function onItemClick, which add count and item price

--- src/App.tsx
+++ src/App.tsx
@@ -25,21 +25,59 @@ const ITEMS: Items[] = [
 ];
 
 const App = ()  => {
-  const [order, setOrder] = useState(['Order is empty please! Please add some items!']);
+  const [order, setOrder] = useState(['Order is empty! Please add some items!']);
   const [items, setItems] = useState([
-      {name: 'Latte', count: 0, id: nanoid()},
-      {name: 'Pasta', count: 0, id: nanoid()},
-      {name: 'Pizza', count: 0, id: nanoid()},
-      {name: 'Ice-cream', count: 0, id: nanoid()},
-      {name: 'Fries', count: 0, id: nanoid()},
-      {name: 'Juice', count: 0, id: nanoid()},
+      {name: 'Latte', count: 0, id: nanoid(), price: 150},
+      {name: 'Pasta', count: 0, id: nanoid(), price: 300},
+      {name: 'Pizza', count: 0, id: nanoid(), price: 350},
+      {name: 'Ice-cream', count: 0, id: nanoid(), price: 100},
+      {name: 'Fries', count: 0, id: nanoid(), price: 160},
+      {name: 'Juice', count: 0, id: nanoid(), price: 90},
   ]);
   const [price, setPrice] = useState([0]);
+
+  const onItemClick = (id:number) => {
+      const orderCopy = [...order];
+      orderCopy[0] = '';
+
+      const itemsCopy = [...items];
+      const itemsObjCopy = {...itemsCopy[id]};
+
+
+      itemsObjCopy.count = itemsObjCopy.count + 1;
+
+      if(itemsObjCopy.count > 1) {
+          itemsObjCopy.price = itemsObjCopy.price + ITEMS[id].price;
+      }
+
+      itemsCopy[id] = itemsObjCopy;
+      setItems(itemsCopy);
+      setOrder(orderCopy);
+  };
+
+  const itemsDetails = items.map((item, index) => {
+      if(order[0] === '') {
+          if(item.count > 0) {
+              return(
+                  <div key={item.id} className="item-list">
+                      <div>
+                          {item.name}
+                          <span> x{item.count}</span>
+                          <div className="item-price"><b>{item.price} KGS</b></div>
+                      </div>
+                      <div>
+                          <button type="button" className="item-delete"></button>
+                      </div>
+                  </div>
+              );
+          }
+      }
+  });
 
   const itemBtns = items.map((ingredient, index) => {
      return(
          <div key={ingredient.id}>
-             <button className="btn">
+             <button className="btn" onClick={() => onItemClick(index)}>
                  <img className="btn-img" alt={ingredient.name} src={ITEMS[index].image}/>
                  <span>
                      {ingredient.name}
@@ -50,12 +88,12 @@ const App = ()  => {
      );
   });
 
-
   return (
     <div className="App">
       <div className="order-block">
           <h2 className="order-title">Order details:</h2>
           {order[0]}
+          {itemsDetails}
       </div>
       <div>
           <h2 className="items-title">Add items:</h2>
